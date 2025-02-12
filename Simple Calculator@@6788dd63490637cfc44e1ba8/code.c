68f40a4175e995d256f868655095550a20096167
Niharika Batra <nihaarikaa03@gmail.com>
2025-02-12 20:16:50 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,22 +1,31 @@
 #include<stdio.h>
 int main(){
-    int num1,num2,operation,sum,diff,prod,div;
-    scanf("%d %d %ch",&num1,&num2,&operation);
+    int num1,num2,sum,diff,prod,div;
+    char operation;
+    scanf("%d %d %c",&num1,&num2,&operation);
     sum=num1+num2;
     diff=num1-num2;
     prod=num1*num2;
     div=num1/num2;
-    if (operation='+'){
+    if (operation=='+'){
         printf("%d",sum);
     }
-    else if(operation='-'){
+    else if(operation=='-'){
         printf("%d",diff);
     }
-    else if(operation='*'){
+    else if(operation=='*'){
         printf("%d",prod);
     }
+    else if(operation=='/'){
+        if(num2==0){
+            printf("Error");
+        }
+        else{
+            printf("%d",div);
+        }
+    }
     else{
-        printf("%d",div);
+        printf("error");
     }
     return 0;
 }
